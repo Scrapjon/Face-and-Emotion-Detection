24905f454d03412c270fda7d7714cf14b3710a48
Oliver Moloney <127998765+Scrapjon@users.noreply.github.com>
2026-05-26 12:32:49 +1000

    Set up video stream as standalone and as frame generator

--- src/face_emotion/face_recognition/model.py
+++ src/face_emotion/face_recognition/model.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
 import time
-
+from threading import Thread
 import cv2
 import pandas as pd
 from deepface import DeepFace
+from dataclasses import dataclass
 
 CAMERA_FPS_CAP = 30
-MAX_DELTA_SECONDS = (1/CAMERA_FPS_CAP) # Yeesh bad naming convention sorry
 
 UNKNOWN_NAME = "WHO ARE YOU???"
+WINDOW_TITLE = "IMAGE RECOGNITION WOAHHHHH!"
 
 
 # ahh maybe needs a better name lol
@@ -17,6 +18,9 @@ class FacialRecognitionModel:
     MODEL_NAME = "Facenet"
     DETECTOR = "opencv"
     DISTANCE_METRIC = "cosine"
+
+    prev_recognitions = []
+    detection_thread: Thread | None = None
 
     def __init__(self, db_path: Path | str = Path("data", "classification_data", "train_data")) -> None:
         self.db_path = db_path
@@ -30,7 +34,6 @@ class FacialRecognitionModel:
         """
         results = []
         try:
-            print(self.db_path)
             # dfs does not stand for deepfaces it stands for dataframes. its confusing ik.
             dfs = DeepFace.find(
                 img_path=frame,
@@ -42,6 +45,7 @@ class FacialRecognitionModel:
                 silent=True
             )
         except: # either no face in frame or no images in DB
+            print("Nothing seen")
             return results
         
         # get bounding box data
@@ -81,32 +85,71 @@ class FacialRecognitionModel:
             cv2.putText(frame, name, (x, label_y), cv2.FONT_HERSHEY_COMPLEX, 0.8, color, 2)
         return frame
     
-    def run_stream(self, camera_index = 0):
-        t1 = 0
+    def detect_and_assign(self, frame):
+        self.prev_recognitions = self.detect(frame)
+    
+    def async_detect(self, frame):
 
-        cap = cv2.VideoCapture(camera_index)
-        if not cap.isOpened():
+        # Filter out NoneType
+        if self.detection_thread is None:
+            self.detection_thread = Thread(None, self.detect_and_assign, args=(frame,))
+            self.detection_thread.start()
+
+        # Check process isn't overlapping
+        elif not self.detection_thread.is_alive():
+            self.detection_thread = Thread(None, self.detect_and_assign, args=(frame,))
+            self.detection_thread.start()
+        
+        return self.draw_boxes(frame, self.prev_recognitions)
+    
+    def run_stream(self, camera_index = 0, standalone = False):
+        """
+        Runs the video stream which is directly connected to the deepface model
+        Args:
+            camera_index (int): The index of the camera on the machine that the program will capture video from.
+            standalone (bool): Determines whether the stream will run as its own application or as a frame generator. Set to False if plugging into ui such as TKinter and True for running standalone. \
+                You need to unpack the function like a generator though to use standalone. \
+                If using standalone = True, its recommended to use the function FacialRecognitionModel.run_standalone()
+        """
+        self.prev_recognitions = []
+        self.detection_active = True
+        if standalone:
+            cv2.namedWindow(WINDOW_TITLE)
+        vc = cv2.VideoCapture(camera_index)
+
+        if vc.isOpened(): # try to get the first frame
+            rval, frame = vc.read()
+        else:
+            rval = False
             raise RuntimeError("NO CAMERA AAAAAAAAH (maybe try a different camera_index value...)")
-        prev_recognitions = []
-        frame_idx = 0
+        
         try:
-            while True:
+            while rval:
+                rval, frame = vc.read()
 
-                if (time.perf_counter() - t1) < MAX_DELTA_SECONDS: continue
-                ok, frame = cap.read()
-                if not ok:
-                    print("failed to grab frame")
-                    break
-                frame_idx += 1
-                
-                prev_recognitions = self.detect(frame)
-                display_frame = self.draw_boxes(frame, prev_recognitions)
-                cv2.imshow('IMAGE RECOGNITION WOAHHHHH!', display_frame)
-                t1 = time.perf_counter()
+                if standalone:
+                    key = cv2.waitKey(20)
+                    if key == 27: # exit on ESC
+                        break
+                    if key == 100: # toggle detection on D
+                        self.detection_active = not self.detection_active
+
+                if self.detection_active:
+                    display_frame = self.async_detect(frame)
+                else:
+                    display_frame = frame
+
+                if standalone:
+                    cv2.imshow(WINDOW_TITLE, display_frame)
+                else:
+                    yield display_frame, 
         finally:
-            cap.release()
-            cv2.destroyAllWindows()
-
-
-
-                
+            vc.release()
+            if standalone:
+                cv2.destroyAllWindows()
+    def run_standalone(self, camera_index = 0):
+        stream = self.run_stream(camera_index, True)
+        next(stream)
+if __name__ == "__main__":
+    model = FacialRecognitionModel(Path("debug_data"))
+    model.run_standalone()
